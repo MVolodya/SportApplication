8c9a49c78d3530bd1ec4a6b6f3ec1e8bb261e8c5
andrii <andriy.serbatyak@gmail.com>
2016-08-26 20:45:18 +0300

    add new refresh layout

--- app/src/main/java/info/androidhive/firebase/Fragments/AllUsersFragment.java
+++ app/src/main/java/info/androidhive/firebase/Fragments/AllUsersFragment.java
@@ -3,15 +3,14 @@ package info.androidhive.firebase.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.widget.Toast;
 
 import com.github.rahatarmanahmed.cpv.CircularProgressView;
 import com.google.firebase.database.DataSnapshot;
@@ -19,18 +18,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.tuesda.walker.circlerefresh.CircleRefreshLayout;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import info.androidhive.firebase.Activities.MainActivity;
 import info.androidhive.firebase.Classes.RatedUser;
 import info.androidhive.firebase.Classes.RecycleViewClasses.AllUsersAdapter;
 import info.androidhive.firebase.Classes.RecycleViewClasses.DividerItemDecoration;
-import info.androidhive.firebase.Classes.RecycleViewClasses.LeagueAdapter;
-import info.androidhive.firebase.Classes.User;
 import info.androidhive.firebase.R;
 
 /**
@@ -42,7 +37,7 @@ public class AllUsersFragment extends Fragment {
     private AllUsersAdapter usersAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView recyclerView;
-    private SwipeRefreshLayout swipeRefreshLayout;
+private CircleRefreshLayout refreshLayout;
     private CircularProgressView progressView;
 
     public AllUsersFragment() {
@@ -57,22 +52,24 @@ public class AllUsersFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_all_users, container, false);
 
         progressView = (CircularProgressView) view.findViewById(R.id.progress_view);
-        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+        refreshLayout = (CircleRefreshLayout) view.findViewById(R.id.refresh_layout_users);
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_users);
         mLayoutManager = new LinearLayoutManager(view.getContext());
 
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        refreshLayout.setOnRefreshListener(
+                new CircleRefreshLayout.OnCircleRefreshListener() {
+                    @Override
+                    public void refreshing() {
+                        getUsers();
+                    }
+                    @Override
+                    public void completeRefresh() {}
+                });
 
         getUsers();
-
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getUsers();
-            }
-        });
 
         return view;
     }
@@ -89,7 +86,7 @@ public class AllUsersFragment extends Fragment {
                 }
                 progressView.stopAnimation();
                 progressView.setVisibility(View.GONE);
-                swipeRefreshLayout.setRefreshing(false);
+                refreshLayout.finishRefreshing();
                 usersAdapter = new AllUsersAdapter(ratedUserList);
                 recyclerView.setAdapter(usersAdapter);
             }
