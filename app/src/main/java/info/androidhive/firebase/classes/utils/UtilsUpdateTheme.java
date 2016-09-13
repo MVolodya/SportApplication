0349c34e66b2fcb1f66c8eb39f448ae5670bc70e
MVolodya <medvid.volodya@gmail.com>
2016-09-13 19:56:55 +0300

    Update rate

---
+++ app/src/main/java/info/androidhive/firebase/classes/utils/UtilsUpdateTheme.java
@@ -0,0 +1,18 @@
+package info.androidhive.firebase.classes.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import info.androidhive.firebase.R;
+
+public class UtilsUpdateTheme {
+    public static void setTheme(Context context, int theme) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        prefs.edit().putInt(context.getString(R.string.prefs_theme_key), theme).apply();
+    }
+    public static int getTheme(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getInt(context.getString(R.string.prefs_theme_key), -1);
+    }
+}
