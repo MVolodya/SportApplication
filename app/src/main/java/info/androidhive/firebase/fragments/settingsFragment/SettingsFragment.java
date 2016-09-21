327de026767619f98d82965593dd90c9b08e494b
andrii <andriy.servatyak@gmail.com>
2016-09-21 14:52:39 +0300

    FINAL VERSION

--- app/src/main/java/info/androidhive/firebase/fragments/settingsFragment/SettingsFragment.java
+++ app/src/main/java/info/androidhive/firebase/fragments/settingsFragment/SettingsFragment.java
@@ -19,7 +19,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
